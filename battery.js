c6d44c2100b1d063ea71ac69fa3ae13b7cbd393a
LakshmanKishore <lakshmankishore17.com>
2021-06-17 20:31:48 +0530

    Fix UI

--- battery.js
+++ battery.js
@@ -1,21 +1,34 @@
 window.onload = function () {
     let equation = document.querySelector(".equation");
+    let timeLeftDisplay = document.querySelector(".timeLeftDisplay");
+    let timeLeft = document.querySelector(".timeLeft");
+    let collectDataButton = document.querySelector(".collectDataButton");
+    let timerDisplay = document.querySelector(".timerDisplay");
+    let timer = document.querySelector(".timer");
+    let predictTimeButton = document.querySelector(".predictTimeButton");
+    let heading = document.querySelector(".heading");
+    let instruction = document.querySelector(".instruction");
+    let predictInstruction = document.querySelector(".predictInstruction");
+    // let dataDisplay = document.querySelector(".data");
 
     //Collecting the data from the user 
+
     let batteryPromise = navigator.getBattery();
     batteryPromise.then(batteryCallback);
 
-    let collectDataButton = document.querySelector(".collectDataButton");
-    let timer = document.querySelector(".timer");
-    let dataOnScreen = document.querySelector(".data");
 
     function batteryCallback(batteryObject) {
         collectDataButton.addEventListener("click", func)
         function func() {
+            collectDataButton.hidden = true;
+            instruction.hidden = true;
+            heading.innerText = "Collecting the Data . . .";
             collectBatteryData(batteryObject);
+            collectDataButton.removeEventListener("click", func);
         }
     }
 
+    //This runs when user clicks on Collect Data button.
     function timerDecrement() {
         min = "2";
         sec = "0";
@@ -42,31 +55,35 @@ window.onload = function () {
         if (!batteryObject.charging) {
             alert("Please connect the charger for collecting the Data");
         } else {
-            timer.hidden = false;
+            timerDisplay.hidden = false;
             timerDecrement();
-            batteryLevel.push(batteryObject.level);
+            batteryLevel.push(batteryObject.level * 100);
             collectData = setInterval(function () {
-                batteryLevel.push(batteryObject.level);
+                batteryLevel.push(batteryObject.level * 100);
                 // minutes = new Date().getTime();
                 // time.push(minutes);
-                console.log(batteryLevel);
-                console.log(time);
-                dataOnScreen.innerText = batteryLevel + "\n" + time;
-            }, 5000)
+                // console.log(batteryLevel);
+                // console.log(time);
+                // dataDisplay.innerText = batteryLevel + "\n" + time;
+            }, 5000);
             setTimeout(function () {
                 clearInterval(collectData);
-                timer.hidden = true;
-            }, 121000)
+                timerDisplay.hidden = true;
+                predictInstruction.hidden = false;
+                predictTimeButton.hidden = false;
+                heading.innerText = "Prediction";
+            }, 121000);
         }
     }
     //time=slope*100(batteryLevel)+b
 
     const trainX = batteryLevel;
+    // const trainX = [4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8];
     // const trainX = [0.55, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.57, 0.58];
     const trainY = time;
     // console.log(trainY);
-    //Prediction logic
 
+    //Prediction logic
     let m = 0;
     let b = 0;
 
@@ -78,18 +95,25 @@ window.onload = function () {
         }
         // console.log(XX);
         // Linear regression using OLS(Ordinary Least Squares)
-        let theta = math.multiply(math.inv(math.multiply(math.transpose(XX), XX)), math.multiply(math.transpose(XX), trainY))
+        let theta = math.multiply(math.inv(math.multiply(math.transpose(XX), XX)), math.multiply(math.transpose(XX), trainY));
 
         m = theta[0];
         b = theta[1];
-        console.log(m, b);
-        equation.innerText = "Y = " + m + "x + " + b
+        // console.log(m, b);
+        // equation.innerText = "Y = " + m + "x + " + b;
+        timeLeftDisplay.hidden = false;
+        let timeSeconds = m * 100 + b;
+        let hours = 0;
+        let minutes = 0;
+        hours = Math.floor(timeSeconds / 60 / 60);
+        minutes = Math.floor(timeSeconds / 60) - (hours * 60);
+        timeLeft.innerHTML = `<b> ${hours} hours ${minutes} minutes </b>`;
     }
 
     function plot() {
         const px = [Math.min.apply(null, trainX), Math.max.apply(null, trainX)];
         const py = px.map(x => (m * x + b));
-        console.log(px, py)
+        // console.log(px, py)
 
         var trace1 = {
             x: trainX,
@@ -103,18 +127,31 @@ window.onload = function () {
             y: py,
             mode: 'lines',
             type: 'scatter'
-        }
+        };
 
         var data = [trace1, trace2];
         // console.log(data);
-        Plotly.newPlot('myDiv', data);
+        Plotly.newPlot('myDiv', data, {
+            xaxis: { zeroline: false, title: 'Battery Percentage' },
+            yaxis: { zeroline: false, title: 'Time Intervals' }
+        });
     }
-    plot();
 
-    let predictTime = document.querySelector(".predictTimeButton");
-    predictTime.addEventListener("click", train);
+    predictTimeButton.addEventListener("click", train);
     function train() {
+        heading.innerText = "Prediction Results";
+        predictInstruction.hidden = true;
+        predictTimeButton.hidden = true;
+        //To check if all the data in trainX is same.
+        let check = new Set(batteryLevel);
+        if (check.size == 1) {
+            predictInstruction.hidden = false;
+            predictInstruction.innerHTML = "Something went wrong. <br> OR <br> Your device is too slow in charging. <br> My Model cannot predict the time. Sorry :("
+            predictTimeButton.removeEventListener("click", train);
+            return;
+        }
         linearRegression();
-        predictTime.removeEventListener("click", trainWithEpoch);
+        plot();
+        predictTimeButton.removeEventListener("click", train);
     }
 }
